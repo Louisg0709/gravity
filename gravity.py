3c2981bfd15b1e5fee71ef0fde3d6c4de87f2bcd
Louisg0709 <31897191+Louisg0709@users.noreply.github.com>
2023-01-16 20:17:51 +0000

    Acceleration due to gravity can now be calculated!

--- gravity.py
+++ gravity.py
@@ -7,24 +7,39 @@ import math
 G = 6.67*10**-11
 
 class body:
-    mass = 0
-    x = 0
-    y = 0
-    vx = 0
-    vy = 0
-    fx = 0
-    fy = 0
+    def __init__(self,x=0,y=0,mass=100):
+        self.x=x
+        self.y=y
+        self.mass=mass
+        self.vx=0
+        self.vy=0
+        self.fx=0
+        self.fy=0
 
-    def distanceTo(self, x2, y2):
+    def distance_to(self, x2, y2):
+        print(x2,y2)
+        print(self.x,self.y)
         distance = math.sqrt(((self.x-x2)**2)+((self.y-y2)**2))
         return distance
+    
+    def calculate_force(self,body):
+        dist=self.distance_to(body.x, body.y)
+        forcemag=G*self.mass*body.mass/(dist**2)
+        fx=(forcemag/dist)*(body.x-self.x)
+        fy=(forcemag/dist)*(body.y-self.y)    
+        force = [fx,fy]
+        return force
+        
+    def calculate_acceleration(self,force):
+        ax = force[0]/self.mass
+        ay = force[1]/self.mass
+        acceleration = [ax,ay]
+        return acceleration
 
-b1 = body
-b1.x = 5
-b1.y = 5
+b1 = body(0,0,1.989*10**30)
+b2 = body(1.496*10**11,0,5.972*10**24)
 
-b2 = body
-b2.x = 0
-b2.y = 0
+print(b1.calculate_acceleration(b1.calculate_force(b2)))
+print(b2.calculate_acceleration(b2.calculate_force(b1)))
 
-print(b1.distanceTo(b2.x,b2.y))
+
