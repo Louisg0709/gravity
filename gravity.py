b86e98cd5de9488f26947564c6460131e44c5144
Louisg0709 <31897191+Louisg0709@users.noreply.github.com>
2023-01-17 19:11:02 +0000

    Now simulates 50 steps

--- gravity.py
+++ gravity.py
@@ -7,7 +7,7 @@ import math
 G = 6.67*10**-11
 
 class body:
-    def __init__(self,x=0,y=0,mass=100):
+    def __init__(self,x=0,y=0,mass=100,vx=0,vy=0):
         self.x=x
         self.y=y
         self.mass=mass
@@ -17,8 +17,6 @@ class body:
         self.fy=0
 
     def distance_to(self, x2, y2):
-        print(x2,y2)
-        print(self.x,self.y)
         distance = math.sqrt(((self.x-x2)**2)+((self.y-y2)**2))
         return distance
     
@@ -36,10 +34,21 @@ class body:
         acceleration = [ax,ay]
         return acceleration
 
+    def calculate_new_velocity(self,body):
+        velocity = [self.vx,self.vy]
+        acceleration = self.calculate_acceleration(self.calculate_force(body))
+        new_velocity = [velocity[0]+acceleration[0],velocity[1]+acceleration[1]]
+        return new_velocity
+
+    def update(self,body):
+        velocity = self.calculate_new_velocity(body)
+        self.x += velocity[0]
+        self.y += velocity[1]
+        print(self.x,self.y,"\n")
+
 b1 = body(0,0,1.989*10**30)
-b2 = body(1.496*10**11,0,5.972*10**24)
+b2 = body(1.496*10**11,0,5.972*10**24,20000,0)
 
-print(b1.calculate_acceleration(b1.calculate_force(b2)))
-print(b2.calculate_acceleration(b2.calculate_force(b1)))
-
-
+for i in range(50):
+    b1.update(b2)
+    b2.update(b1)
